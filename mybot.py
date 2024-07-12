2f796cd28887b61303818cf4359b41002d36706a
LuckyLuc96 <lucsaybibi@gmail.com>
2024-07-12 12:33:40 -0400

    Profile setup completed to what I wanted. Now I am working on having it search for jobs - which is working. Next step is to apply to these jobs on my behalf.

--- mybot.py
+++ mybot.py
@@ -5,15 +5,25 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver import FirefoxProfile
 import time
 
-profile_path = "/home/lucky1/.mozilla/firefox/zjpezclp.default-release"
+profile_path = "/home/lucky1/.mozilla/firefox/uhc9pf1j.Selenium"
 geckodriver_path = "/home/lucky1/.cache/selenium/geckodriver/linux64/0.34.0/geckodriver"
 options = webdriver.FirefoxOptions()
 options.profile = webdriver.FirefoxProfile(profile_path)
 driver = webdriver.Firefox(options=options)
 
-driver.get("http://www.python.org/")
+driver.get("http://www.indeed.com/")
+print("Website opened: ", driver.title)
+
+search_title = driver.find_element(By.ID, "text-input-what")
+search_location = driver.find_element(By.ID, "text-input-where")
+
+search_title.clear()
+search_title.send_keys("administrative")
+search_location.clear()
+search_location.send_keys("Remote")
+search_location.send_keys(Keys.RETURN)
 
 
 
-#time.sleep(2)
-#driver.quit()
+time.sleep(3)
+driver.quit()

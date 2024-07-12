982b943607f076bcdef40a6744628bc18773b407
LuckyLuc96 <lucsaybibi@gmail.com>
2024-07-11 23:56:43 -0400

    profile is working with this. However the initialization of the entire profile is too long. I'll build a new profile for the bot instead

--- mybot.py
+++ mybot.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver import FirefoxProfile
 import time
 
 profile_path = "/home/lucky1/.mozilla/firefox/zjpezclp.default-release"
 geckodriver_path = "/home/lucky1/.cache/selenium/geckodriver/linux64/0.34.0/geckodriver"
-options = Options()
-profile = FirefoxProfile(profile_path)
-options.Profile = profile
-driver = webdriver.Firefox(options)
+options = webdriver.FirefoxOptions()
+options.profile = webdriver.FirefoxProfile(profile_path)
+driver = webdriver.Firefox(options=options)
 
 driver.get("http://www.python.org/")
 

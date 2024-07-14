e399a664e3b9d29e43212e62ea3390510da141f1
LuckyLuc96 <lucsaybibi@gmail.com>
2024-07-13 20:25:54 -0400

    Indeed applications are underway. Currently I think the issue in the code is that selenium is not understanding that a new tab is being opened when I hit "apply now." I'll need to teach it to recognize the new tab so I can proceed.

--- jobbot.py
+++ jobbot.py
@@ -5,51 +5,62 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver import FirefoxProfile
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
 options = webdriver.FirefoxOptions()
 options.profile = webdriver.FirefoxProfile(profile_path)
 driver_service = Service(executable_path=geckodriver_path)
-driver = webdriver.Firefox(options=options, service = driver_service) #options(or profile name) is correcct. The gecko driver is not working on the device I'm using right now.
+driver = webdriver.Firefox(options=options, service = driver_service)
 
 
 driver.get("http://www.indeed.com/")
 print("Website opened: ", driver.title)
+try:
+    search_title = driver.find_element(By.ID, "text-input-what")
+    search_location = driver.find_element(By.ID, "text-input-where")
 
-search_title = driver.find_element(By.ID, "text-input-what")
-search_location = driver.find_element(By.ID, "text-input-where")
-
-search_title.clear()
-search_title.send_keys("administrative")
-search_location.clear()
-search_location.send_keys("Remote")
-search_location.send_keys(Keys.RETURN)
-
-
-
-time.sleep(3)
-driver.quit()
-
-profile_path = "/home/lucky1/.mozilla/firefox/uhc9pf1j.Selenium"
-geckodriver_path = "/home/lucky1/.cache/selenium/geckodriver/linux64/0.34.0/geckodriver"
-options = webdriver.FirefoxOptions()
-options.profile = webdriver.FirefoxProfile(profile_path)
-driver = webdriver.Firefox(options=options)
-
-driver.get("http://www.indeed.com/")
-print("Website opened: ", driver.title)
-
-search_title = driver.find_element(By.ID, "text-input-what")
-search_location = driver.find_element(By.ID, "text-input-where")
-
-search_title.clear()
-search_title.send_keys("administrative")
-search_location.clear()
-search_location.send_keys("Remote")
-search_location.send_keys(Keys.RETURN)
-
+    searchname = "administrative"
+    searchlocation = "remote"
+    search_title.clear()
+    search_title.send_keys(searchname)
+    search_location.clear()
+    search_location.send_keys(searchlocation)
+    search_location.send_keys(Keys.RETURN)
+    print(f"Search of {searchname} with the location set to {searchlocation}")
+    time.sleep(3)
+    apply_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'jobsearch-IndeedApplyButton-newDesign') and text()='Apply now' and not(@aria-label='Apply now (opens in a new tab)')]"))
+    )
+    # These are the actions of clicking the above buttons
+    apply_button.click()
+    time.sleep(4)
+    resume_selection = WebDriverWait(driver, 4).until(
+        EC.presence_of_element_located((By.XPATH, "//span[text()='Indeed Resume']"))
+    )
+    continue_button = WebDriverWait(driver, 4).until(
+        EC.presence_of_element_located((By.XPATH, "//span[text()='Continue']"))
+    )
+    resume_selection.click()
+    time.sleep(0.5)
+    continue_button.click()
+    time.sleep(3)
+    continue_button2 = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[text()='Continue']"))
+    )
+    continue_button2.click()
+    time.sleep(3)
 
 
-time.sleep(3)
-driver.quit()
+
+
+
+except Exception as e:
+    print("There has been an error. See: ", e)
+
+finally:
+    time.sleep(10)
+    driver.quit()
+    print("Browser closed!")

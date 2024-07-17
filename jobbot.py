c31935538eb6e7a78c2a461b88bf4cca9cc2eae3
LuckyLuc96 <lucsaybibi@gmail.com>
2024-07-17 18:17:30 -0400

    A lot of progress was made and a lot of code written. It is 80% there actually submitting an application, assuming the employer is not asking many questions. Will continue to fine tune. Goal is to submit 1 application from start to finish.

--- jobbot.py
+++ jobbot.py
@@ -9,16 +9,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
 options = webdriver.FirefoxOptions()
 options.profile = webdriver.FirefoxProfile(profile_path)
 driver_service = Service(executable_path=geckodriver_path)
 driver = webdriver.Firefox(options=options, service = driver_service)
 
 
-driver.get("http://www.indeed.com/")
-print("Website opened: ", driver.title)
 try:
+    sleeptime = 10
+    #int(input("How slow is the internet today? In seconds: \n"))
+    print(f"Okay, great! Starting up. Some parts of the program will wait {sleeptime} seconds to load.")
+    print("For now this program is still being made, but it is planned to be ran in 'headless' mode which means you won't see the window.")
+
+    driver.get("http://www.indeed.com/")
+    print("Website opened: ", driver.title)
+
     search_title = driver.find_element(By.ID, "text-input-what")
     search_location = driver.find_element(By.ID, "text-input-where")
 
@@ -34,33 +39,73 @@ try:
     apply_button = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'jobsearch-IndeedApplyButton-newDesign') and text()='Apply now' and not(@aria-label='Apply now (opens in a new tab)')]"))
     )
-    # These are the actions of clicking the above buttons
+    print("Apply button found!:",apply_button.get_attribute('innerHTML'))
+    driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", apply_button)
+
     apply_button.click()
-    time.sleep(4)
-    resume_selection = WebDriverWait(driver, 4).until(
+    print("Apply button clicked!")
+    time.sleep(sleeptime)
+    driver.switch_to.window(driver.window_handles[1])
+    print("Switching selenium to focus on new tab opened by applying")
+    resume_selection = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//span[text()='Indeed Resume']"))
     )
-    continue_button = WebDriverWait(driver, 4).until(
-        EC.presence_of_element_located((By.XPATH, "//span[text()='Continue']"))
-    )
+    print("Resume selection button found!:",resume_selection.get_attribute('innerHTML'))
+
     resume_selection.click()
-    time.sleep(0.5)
-    continue_button.click()
-    time.sleep(3)
-    continue_button2 = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//span[text()='Continue']"))
+    print("Resume selected!")
+    time.sleep(2)
+    continue_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[contains(span/text(), 'Continue')]"))
     )
-    continue_button2.click()
-    time.sleep(3)
+    print("Continue button found!:",continue_button.get_attribute('innerHTML'))
+    if not continue_button.is_displayed(): #Will scroll down to the button, if button is not visible
+        driver.execute_script("arguments[0].scrollIntoView();", continue_button)
+    time.sleep(1)
+    continue_button.click()
+    print("continued button selected!")
+    print(f"Waiting {sleeptime} seconds.")
+    time.sleep(sleeptime)
 
+    #This is where it asks for a relevant job.
+    continue_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[contains(span/text(), 'Continue')]"))
+    )
+    continue_button.click()
+    #This is where it asks employer questions. I suspect the most problems from this section in the future.
+    continue_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[contains(span/text(), 'Continue')]"))
+    )
+    continue_button.click()
+    #This is asking if I have certain qualifications. I should make a loop for this..
+    continue_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[contains(span/text(), 'Continue')]"))
+    )
+    continue_button.click()
 
+    #This is Indeed saying I am not qualified. Should I continue applying? Of course.
+    continue_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[contains(span/text(), 'Continue applying')]"))
+    )
+    if not continue_button.is_displayed(): #Will scroll down to the button, if button is not visible
+        driver.execute_script("arguments[0].scrollIntoView();", continue_button)
+    continue_button.click()
+
+    #This is where you submit your application!
+    submit_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[contains(span/text(), 'Submit your application')]"))
+    )
+    if not submit_button.is_displayed(): #Will scroll down to the button, if button is not visible
+        driver.execute_script("arguments[0].scrollIntoView();", submit_button)
+        submit_button.click()
 
 
 
 except Exception as e:
     print("There has been an error. See: ", e)
+    driver.close()
+    time.sleep(5)
 
 finally:
-    time.sleep(10)
     driver.quit()
     print("Browser closed!")
